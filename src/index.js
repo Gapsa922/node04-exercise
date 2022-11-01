d5ca417a3d41394dd0ddb34152475392354d8a19
Gapsa922 <goncalo_dsilva@hotmail.com>
2022-11-01 12:42:36 +0000

    solve the quest

--- src/index.js
+++ src/index.js
@@ -1,17 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+require("dotenv").config();
+const name = process.env.MY_NAME;
+const city = process.env.MY_CITY;
+const language = process.env.MY_LANGUAGE;
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+console.log(`I am ${name}, wilder in ${city}, and I love ${language}.`);
